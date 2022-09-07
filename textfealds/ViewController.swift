3ff5d25543b9aa138bf051447627525215b1f7bc
Esmay <Molinaesme729@gmail.com>
2022-09-07 11:00:26 -0600

    mvp

--- textfealds/ViewController.swift
+++ textfealds/ViewController.swift
@@ -8,12 +8,25 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    
+//outlets and veribolls go up at the top
+    @IBOutlet weak var bruLaible: UILabel!
+    @IBOutlet weak var hoeboText: UITextField!
+    var x="" //the uesd qotes for words
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
+    
+    //akshens and functions are at the bottom
 
-
+    @IBAction func yolo(_ sender: Any) {
+        x = hoeboText.text ?? "" //?? is an optional. gives a difolt value so the app dousent crash
+        bruLaible.text = x
+        
+    }
+    
 }
 
